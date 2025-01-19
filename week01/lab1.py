d4450350581c0f7bd635e8073068216aadf0685a
JasleenKaurSaini <101486141@georgebrown.ca>
2025-01-18 22:38:48 -0500

    first commit

---
+++ week01/lab1.py
@@ -0,0 +1,22 @@
+# Sample Coding Questions 01 Week 01
+# Name: Jasleen Kaur Saini
+
+# Defining Variables
+my_array = [1, 4, 7, 9]
+
+# Order of Operations
+a, b, c, d = 1, 2, 3, 4
+
+# Fully-Bracketed Version of the expression
+# e = a - b ** c // d + a % c
+# Fully-bracketed version:
+# e = ((a - (b ** c)) // d) + (a % c)
+
+# Formatting
+temperature = 32.6
+print(f"The temperature today is: {temperature:.3f} degrees Celsius")
+
+# Common Functions
+userAge = int(input("Please enter your age: "))
+userAge = userAge + 22
+print(f"Now showing the shop items filtered by age: {userAge}")
